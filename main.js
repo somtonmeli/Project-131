4e8cd5a911b324e347e3fdb4ea53729814d64a20
somtonmeli <115658287+somtonmeli@users.noreply.github.com>
2024-02-17 17:12:05 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function bottle()
+{
+    window.location = "bottle.html";
+}
+
+function bed()
+{
+    window.location = "bed.html";
+}
+
+function table()
+{
+    window.location = "table.html";
+}
+
+function chair()
+{
+    window.location = "chair.html";
+}
+
+function fruits()
+{
+    window.location = "fruits.html";
+}
+
+function home()
+{
+    window.location = "index.html";
+}
